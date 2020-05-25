965e7c0dd844837771359b5d7949e6ff5f3c8b60
hajam09 <hajam09@yahoo.com>
2020-05-25 19:47:20 +0100

    forum page

--- forum/views.py
+++ forum/views.py
@@ -3,5 +3,8 @@ from .models import Forum, Comment
 
 def forum_mainpage(request):
 	all_forums = Forum.objects.all()
-	context = {"all_forums": all_forums}
+	all_comments = Comment.objects.all()
+	for i in all_forums:
+		print(Comment.objects.filter(forum=i))
+	context = {"all_forums": all_forums, "all_comments": all_comments}
 	return render(request, "forum_mainpage.html", context)
